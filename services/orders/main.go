4ed20d43f5dcd381a9d456fb88942b7d34a299f1
Duc Tran <tranaduc9x@gmail.com>
2025-11-03 22:58:16 +0700

    setup order http server

--- services/orders/main.go
+++ services/orders/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/ductran999/letobserv/services/orders/handler"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	log.Println("orders service")
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
+	hdl := handler.NewOrderHandler(nil)
+
+	// Simple api place an order
+	r := gin.Default()
+	r.GET("/health", hdl.CheckHealth)
+
+	// Start http server
+	address := "0.0.0.0:" + os.Getenv("ORDER_SERVICE_PORT")
+	log.Println("[INFO] product service serving on", os.Getenv("ORDER_SERVICE_PORT"))
+	if err := r.Run(address); err != nil {
+		log.Println("[ERR] start product service error", err)
+	}
 }
