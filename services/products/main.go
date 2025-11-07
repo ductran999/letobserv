98a9d6ee72efe2616c9c553fa83e2568de114067
Duc Tran <tranaduc9x@gmail.com>
2025-11-07 22:27:48 +0700

    add otel intrustment

--- services/products/main.go
+++ services/products/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/ductran999/dbkit"
 	"github.com/ductran999/letobserv/services/common"
@@ -12,13 +14,74 @@ import (
 	"github.com/ductran999/letobserv/services/products/usecase"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+	"google.golang.org/grpc/credentials"
 )
 
-func main() {
+func init() {
 	// Load .env file
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using system environment variables")
 	}
+}
+
+var (
+	serviceName  = os.Getenv("PRODUCT_SERVICE_NAME")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	insecure     = os.Getenv("INSECURE_MODE")
+)
+
+func initTracer() func(context.Context) error {
+	var secureOption otlptracegrpc.Option
+
+	if strings.ToLower(insecure) == "false" || insecure == "0" || strings.ToLower(insecure) == "f" {
+		secureOption = otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	} else {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to create exporter: %v", err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(serviceName),
+			semconv.TelemetrySDKLanguageGo,
+		),
+	)
+	if err != nil {
+		log.Fatalf("Could not set resources: %v", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+
+	return exporter.Shutdown
+}
+
+func main() {
+	cleanup := initTracer()
+	defer cleanup(context.Background())
 
 	// ConnectDB
 	port, err := strconv.Atoi(os.Getenv("DB_PRODUCT_PORT"))
@@ -52,6 +115,7 @@ func main() {
 
 	// Simple reduce stock API
 	r := gin.Default()
+	r.Use(otelgin.Middleware(serviceName))
 	r.PATCH("/api/products/:id/reduce-stock", hdl.ReduceProductStock)
 	r.GET("/health", hdl.CheckHealth)
 
