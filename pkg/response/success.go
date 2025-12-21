8bf179467c920d0c785b4e1b0a27c445c8ddab07
Duc Tran <tranaduc9x@gmail.com>
2025-12-21 12:52:29 +0700

    update(pkg): add struct response created

--- pkg/response/success.go
+++ pkg/response/success.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/ductran999/letobserv/pkg/xcontext"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,7 +21,18 @@ func OK(c *gin.Context, data any, message ...string) {
 		Success: true,
 		Data:    data,
 		Message: &msg,
-		TraceID: c.GetString("trace_id"),
+		TraceID: c.GetString(xcontext.TraceIDKey),
 	}
 	c.JSON(http.StatusOK, resp)
+}
+
+func Created(c *gin.Context, data any, message ...string) {
+	msg := strings.Join(message, ", ")
+	resp := SuccessResp{
+		Success: true,
+		Data:    data,
+		Message: &msg,
+		TraceID: c.GetString(xcontext.TraceIDKey),
+	}
+	c.JSON(http.StatusCreated, resp)
 }
