f86a2eb564b661d856f7065f5e40275f8d2044d9
Duc Tran <tranaduc9x@gmail.com>
2026-01-02 14:05:08 +0700

    docs(api): add inventory reservation api contract

--- api/configs/generate.go
+++ api/configs/generate.go
@@ -4,4 +4,4 @@ package generated
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config orders.types.yaml ../schemas/orders.yaml
 
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config inventory.server.yaml ../openapi/inventory.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config inventory.types.yaml ../schemas/products.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config inventory.types.yaml ../schemas/inventory.yaml
